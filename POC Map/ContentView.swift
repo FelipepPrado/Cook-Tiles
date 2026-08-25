b226ae918d4a493612431b1ab47b7faf595384cb
FelipepPrado <felipepdelimaa@gmail.com>
2026-08-25 07:34:53 -0300

    Initial Commit

---
+++ POC Map/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  POC Map
+//
+//  Created by Felipe Prado de Lima on 25/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ POC Map/POC_MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  POC_MapApp.swift
+//  POC Map
+//
+//  Created by Felipe Prado de Lima on 25/08/26.
+//
+
+import SwiftUI
+
+@main
+struct POC_MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
