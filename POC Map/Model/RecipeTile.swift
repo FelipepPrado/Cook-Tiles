37796369ccc506add1141b4e60d9cafe79583f94
FelipepPrado <felipepdelimaa@gmail.com>
2026-08-28 13:19:07 -0300

    feat: tile status

--- POC Map/Model/RecipeTile.swift
+++ POC Map/Model/RecipeTile.swift
@@ -4,9 +4,13 @@ import SpriteKit
 final class RecipeTile {
     let recipe: Recipe
     let tile: SKSpriteNode
+    let row: Int
+    let col: Int
     
-    init(recipe: Recipe, tile: SKSpriteNode) {
+    init(recipe: Recipe, tile: SKSpriteNode, row: Int, col: Int) {
         self.recipe = recipe
         self.tile = tile
+        self.row = row
+        self.col = col
     }
 }
