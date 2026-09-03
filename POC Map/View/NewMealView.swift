c8be1fd51163296dd9617ad8f055fc26f3115b47
FelipepPrado <felipepdelimaa@gmail.com>
2026-09-03 12:07:24 -0300

    feat: add recipe to the new Meal

--- POC Map/View/NewMealView.swift
+++ POC Map/View/NewMealView.swift
@@ -42,12 +42,37 @@ struct NewMealView: View {
                     StarRatingInputComponent(rating: $viewModel.newMeal.stars)
                 }
                 .padding(.top, 16)
-                VStack{
+                VStack(alignment: .leading, spacing: 20){
+                    Text("Receitas do Prato")
+                        .font(Font.custom("Hammersmith One", size: 16, relativeTo: .callout))
+                        .padding(.top, 26)
+                        .border(Color.gray, width: 1)
                     
+                    ForEach(RecipeCategory.allCases) { category in
+                        if !viewModel.newMeal.recipes.contains(where: { $0.category == category }){
+                            Button("Add Receita" ,systemImage: "plus") {
+                                viewModel.recipeCategory = category
+                            }
+                        }
+                        else{
+                            Text(category.displayName)
+                        }
+                    }
                 }
             }
             .padding()
         }
+        .sheet(item: $viewModel.recipeCategory) { category in
+            AddRecipetoMealView(
+                viewModel: AddRecipetoMealViewModel(
+                    viewModel: viewModel,
+                    recipes: recipeModel.filter { recipe in
+                        recipe.category == category && recipe.status == .unlocked
+                    }
+                )
+            )
+        }
+        
         .navigationTitle("Cadastrar Refeição")
         .navigationBarTitleDisplayMode(.inline)
     }
