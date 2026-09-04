130056267f1ee792bce4c2244b03bc92ec23436d
mariafernandabq <mariafernandabqueiroz@gmail.com>
2026-09-04 17:34:59 -0300

    feat: create StepsScreenComponent

---
+++ POC Map/View/Components/StepsScreenComponent.swift
@@ -0,0 +1,42 @@
+import SwiftUI
+
+struct StepsScreenComponent: View {
+    
+    let step: RecipeStep
+    
+    var body: some View {
+        
+        ZStack{
+            Rectangle()
+                .fill(.cream200)
+                .frame(maxWidth: 386, minHeight: 83)
+                .cornerRadius(10)
+            
+            HStack(alignment: .center, spacing: 15){
+                VStack{
+                    Text("\(step.order)ª")
+                        .font(Font.largeTitle.bold())
+                        .foregroundStyle(.green500)
+                    
+                    Text("Etapa")
+                        .font(Font.body.bold())
+                        .foregroundStyle(.brown200)
+                }
+                
+                .padding(20)
+                
+                Text("\(step.instruction)")
+                    .font(Font.headline)
+                    .foregroundStyle(.brown200)
+                
+                Spacer()
+            }
+            .padding(.leading, 10)
+           
+        }
+    }
+}
+    
+    #Preview {
+        StepsScreenComponent(step: RecipeStep(order: 1, instruction: "Finalize com chocolate granulado por cima e leve à geladeira por pelo menos 4 horas.", process: ""))
+    }
