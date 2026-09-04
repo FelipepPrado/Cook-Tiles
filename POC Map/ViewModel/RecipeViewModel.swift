7cd61f9cefbaecab23a401932fa3ec623da59242
Ana14Soares <ana.soarex14@gmail.com>
2026-09-04 08:53:28 -0300

    feat: recipe view

--- POC Map/ViewModel/RecipeViewModel.swift
+++ POC Map/ViewModel/RecipeViewModel.swift
@@ -1,1 +1,20 @@
 
+import Observation
+import AVFoundation
+internal import SpriteKit
+
+
+@Observable
+final class RecipeViewModel{
+    let recipe: Recipe
+
+    
+    init(recipe: Recipe) {
+        self.recipe = recipe
+
+        
+    }
+
+    
+
+}
